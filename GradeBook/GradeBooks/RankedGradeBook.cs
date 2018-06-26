15b2a213185b0403ad8149175fa2c670e6603526
shjulberg <shjulberg@gmail.com>
2018-06-26 16:16:32 -0500

    Grade Book #3
    Tasks 1,2,3,4,5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,43 @@ namespace GradeBook.GradeBooks
         {
             this.Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            int NumberOfStudents = Students.Count;
+            int NumberOfStudentsPerGrade = (int)Math.Round((double)(NumberOfStudents * .2));
+            List<double> GradeList = new List<double>();
+            foreach (Student student in Students)
+            {
+                GradeList.Add(student.AverageGrade);
+            }
+            GradeList.Sort((a, b) => -1 * a.CompareTo(b));
+            double LowestA = GradeList[NumberOfStudentsPerGrade - 1];
+            double LowestB = GradeList[(NumberOfStudentsPerGrade * 2) - 1];
+            double LowestC = GradeList[(NumberOfStudentsPerGrade * 3) - 1];
+            double LowestD = GradeList[(NumberOfStudentsPerGrade * 4) - 1];
+            if (averageGrade >= LowestA)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= LowestB)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= LowestC)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= LowestD)
+            {
+                return 'D';
+            }
+            else
+                return 'F';
+        }
     }
 }
